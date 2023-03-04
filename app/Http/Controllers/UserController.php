a587f81a7b34746b4017c3f3eafce85d286ad90f
Adrián Jurado <adrianjjvv@gmail.com>
2023-03-04 01:06:11 +0100

    Hash contraseña y mejor validación

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -27,15 +29,15 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required',
-            'email' => 'required | unique:users',
-            'password' => 'required',
+            'name' => 'required|max:255|string',
+            'email' => 'required|unique:users|max:255|email|string',
+            'password' => 'required|max:255|string'
         ]);
 
         $user = new User;
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = $request->password;
+        $user->password = Hash::make($request->password);
 
         $user->save();
 
@@ -52,7 +54,7 @@ class UserController extends Controller
     {
         $user = User::find($id);
         if(is_null($user)) {
-            return response()->json('El usuario no existe', 404);
+            return response()->json('User not exist', 404);
         }
     }
 
@@ -66,9 +68,9 @@ class UserController extends Controller
     public function update(Request $request, User $user)
     {
         $request->validate([
-            'name' => 'required',
-            'email' => 'required | unique:users',
-            'password' => 'required',
+            'name' => 'required|max:255|string',
+            'email' => 'required|unique:users|max:255|email|string',
+            'password' => 'required|max:255|string'
         ]);
 
         $user->name = $request->name;
@@ -90,7 +92,7 @@ class UserController extends Controller
     {
         $user = User::find($id);
         if(is_null($user)) {
-            return response()->json('El usuario no existe', 404);
+            return response()->json('User not exist', 404);
         }
 
         $user->delete();
